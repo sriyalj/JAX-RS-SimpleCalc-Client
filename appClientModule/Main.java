be4954d5409693c13baf4a540f4264405c28b8c8
sriyalj <sriyal.jayasinghe@gmail.com>
2020-05-01 23:42:13 +0300

    Finished Get

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -1,49 +1,34 @@
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-
-import org.glassfish.jersey.client.ClientConfig;
+import java.util.Base64;
+import java.util.Scanner;
 
 public class Main {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ClientConfig config = new ClientConfig();
-        Client client = ClientBuilder.newClient(config);
-        String ans = null;
-        Response serverResponse = null;
-        
-        try {
-        	//http://localhost:8081/JerseyCalcService/rest/view/Sriyal/sriyal123/19
-        
-        	WebTarget webTarget = client.target("http://localhost:8081/JerseyCalcService/rest");
-        	serverResponse = webTarget.path("view").path("Sriyal").path("sriyal123").path("19").request().accept(MediaType.TEXT_PLAIN).get();
-        	ans = serverResponse.readEntity(String.class);
-        	int code = serverResponse.getStatus();
-        	System.out.println (ans);
-        	System.out.println (code);
-        	System.out.println (" ");
-        	
-        	      	
-        	webTarget = client.target("http://localhost:8081/JerseyCalcService/rest");
-        	serverResponse = webTarget.path("view").path("Himasha").path("sriyal123").path("19").request().accept(MediaType.TEXT_PLAIN).get();
-        	ans = serverResponse.readEntity(String.class);
-        	code = serverResponse.getStatus();
-        	System.out.println (ans);
-        	System.out.println (code);
-        	
-        	
-        	//ans = webTarget.path("view").path("Sriyal").path("sriyal123").path("19").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-        }
-        catch (Exception e) {
-        	e.printStackTrace();
-        	//System.out.println ()
-        }
-        
-        //ans = webTarget.path("view").path("Rehansa").path("456Rehansa").path("2").request().accept(MediaType.TEXT_XML).get(String.class);
-        //System.out.println (ans);
+		Scanner scn = new Scanner (System.in);
+		boolean cont= true;	
+		Main clinetApp = null;
+		
+		while (cont) {		
+			System.out.print ("Please Enter Your First Number : ");
+			String fstNum = scn.next();
+			System.out.print ("\nPlease Enter Your Second Number :");
+			String scndNum = scn.next();
+			System.out.print ("\nPlease Enter Your Operation :");
+			String operation = scn.next();
+			System.out.print ("\nPlease Enter Your Method :");
+			String method = scn.next();
+			
+			String encodedOperation = Base64.getEncoder().encodeToString(operation.getBytes());			
+			Connection con =  new Connection ();
+			
+			String ans = con.getResponseFromServer(fstNum, scndNum, encodedOperation);			
+			System.out.println (ans);		
+			
+			System.out.print ("\nDo You Wish To Continue [true/false] :");
+			cont = scn.nextBoolean();
+			System.out.println ("");
+		}
 	}
 
 	/* (non-Java-doc)
